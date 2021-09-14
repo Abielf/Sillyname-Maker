290c2a1d66474e401e08bec65f17b2020af71d85
Abielf <abielf@rocketmail.com>
2021-09-14 03:45:05 -0400

    testing

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const nameBank={"First":[
+    "Ava", "Amelia", "Aiden", "Abigail", "Alexander", "Mortimer", "Emma", 
+    "Evelyn", "Elijah", "Ezra", "Ethan", "Luna", "Liam", "Lucas", 
+    "Logan","Oliver", "Owen", "Sebastian", "Samuel", "Karl", "Harley", 
+    "Archibald", "Icabod", "Christopher", "Timothy", "Jeremy", "Cynthia", "Anthony"
+],
+"Last":["Bennett", "Sanchez", "McGillicutty", "Johansson", "Hernandez", "Henderson","Jenkins",
+    "Murphy", "Patterson", "Jackson", "Nelson", "Cox", "Thompson", "Havoc", 
+    "Fubbins", "Jarvis","Dixon", "Sanders", "Hayes", "Collins", "Campbell",
+    "Johnson", "Douglas", "Fitzgerald", "Davis", "Taylor", "Moore", "Harris"
+],
+"Middle":[ 
+    "Fudge", "Corncob", "Jicima", "Gummy Bear", "Porterhouse", "Thunder", "Plums", 
+    "Hairspray","Potato Chip", "Lard", "Tuna Can", "Candied Beets", "Jelly bean", "Wax apple",
+    "Pickles", "Lip Balm", "Umbrella Stand", "Hamster Food", "Spam", "Ladyfingers", "Tortelinni"
+]}
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {nameBank} from "./data";
+
+
+
+class Generator extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            names: nameBank,
+            nameShown: "Spin the wheel, Make a name!"
+        };
+    }
+    randClick(){
+        const rand1=Math.floor(Math.random()*27)
+        const rand2=Math.floor(Math.random()*27)
+        const rand3=Math.floor(Math.random()*20)
+
+        this.setState({
+            nameShown: this.state.names.First[rand1]+" "+ this.state.names.Middle[rand2]+" "+this.state.names.Last[rand3]
+        });
+    }
+
+
+    render(){
+        return(
+            <div>
+                <h2>Lets get a silly name!</h2>
+                <div><button onClick={() => this.randClick()}>Generate name</button></div>
+                <div>{this.state.nameShown}</div>
+            </div>);
+        }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Generator/>,
+    document.getElementById('root')
+);
